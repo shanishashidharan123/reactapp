25994f214285eae49b6598637331a2771c16f943
Shani Shashidharan <67774693+shanishashidharan123@users.noreply.github.com>
2022-05-12 23:17:15 +0530

    app updates
    app updated

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Github</code> and React JS.
+          <code>Github</code> vs React JS.
         </p>
         <a
           className="App-link"
