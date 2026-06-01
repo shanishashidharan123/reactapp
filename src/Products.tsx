587d071cb76b37145867af89fc30d22984342d2a
Shani Shashidharan <fore.shani@gmail.com>
2026-06-01 10:05:29 +0530

    Update Products.tsx

--- src/Products.tsx
+++ src/Products.tsx
@@ -68,13 +68,6 @@ import React from "react";
               </div>
               <div className="cols-70">
                 <p>HTML Static Template, Editable template, HTML 5, CSS3, SCSS, jQuery, Bootstrap 4.0, Editable Logo PSD and other images PSD, Editable Canvas files, Responsive modals, Ready-made editable stylesheets. Healthcare demographics and demo theory from Indian Health Guru his very own beginner website. This Medical website is for all over India, publish worldwide, and brings patients from worldwide for surgery.</p>
-                <a href="https://rzp.io/l/healthGuru" id="rzp-btn" rel="noreferrer nofollow">Buy Now</a>
-                <h2 style={{margin: "24px 0px 12px"}}>Add a Product Review</h2>
-                 {/* TrustBox widget - Review Collector  */}
-<div className="trustpilot-widget" data-locale="en-US" data-template-id="56278e9abfbbba0bdcd568bc" data-businessunit-id="6315fc26c290dfa09b0c16a1" data-style-height="52px" data-style-width="100%">
-  <a href="https://www.trustpilot.com/review/shanishashidharan.co.in" target={"_blank"} rel="noreferrer">Review on Trustpilot</a>
-</div>
- {/* End TrustBox widget  */}
               </div>
             </div>
             <p>Make India and abroad proud. Jai Hind!</p>
@@ -87,8 +80,6 @@ import React from "react";
             </div>
             <div className="cols-70">
               <p>Graphic Designs package at Rs. 25000 per month, this package includes any Graphic Designs listed on the image except Video Editing and Compositing. You can also order products individually.</p>
-             <a href="https://rzp.io/l/ssgraphics" id="rzp-btn" rel="noreferrer nofollow">Order Now</a>
-                <p style={{fontSize: "small"}}>For more information, Graphic solutions or contract, <a href="mailto:shanishashidharan@gmail.com" title="Graphic Queries via email">email us</a>.</p>
                 </div>
                 </div>
           </div>
@@ -104,21 +95,7 @@ import React from "react";
             <p style={{color: "#69631b", lineHeight: "normal"}}><strong>Website accessibility (<abbr title="Web Content Accessibility Guidelines"><em>WCAG</em></abbr>) -</strong> Rs. 37,800 per webpage</p>
             <p style={{color: "#69631b", lineHeight: "normal"}}>Rs. 6000 to Rs. 86000 INR for privacy policy, terms and conditions, disclaimer and website copyrights.</p>
             <p style={{color: "#b22e2e"}}><strong>Note:</strong> <small>Prices may be changed at any time without further notice. We reserve the right to change our products prices at any time without further notice. However, if you have ordered but not yet paid for a product, we guarantee the price for one month from when the order was placed.</small></p>
-            <form action="https://submit-form.com/Maf2UDGw" style={{display: "block"}} data-botpoison-public-key="pk_a3f6cffc-fb62-4f7c-a0fc-d0ad163bb273">
-              <label htmlFor="name">Name
-                <input type="text" name="name" id="name" placeholder="Your Name" required />
-              </label>
-              <label htmlFor="email">Email
-                <input type="email" name="email" id="email" placeholder="Email Address" required />
-              </label>
-              <label htmlFor="phone">Telephone
-                <input type="tel" name="phone" id="phone" placeholder="Telephone Number" required />
-              </label>
-              <label htmlFor="message">Ask and get free quotes
-                <textarea name="message" id="message" placeholder="Your Messages" rows={3} required></textarea>
-              </label>
-              <button type="submit" className="primary">Request</button>
-            </form>
+            <p><a href="mailto:shanishashidharan@hotmail.com" class="btn1">Request</a></p>
           </div>
         </section>
       </>
